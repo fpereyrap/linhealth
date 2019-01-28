d327e684132a43b208e5d22119a6cfe331a2e72d
Facundo Pereyra <f.pereyrap@gmail.com>
2019-01-28 10:48:12 -0300

    first commit

--- linhealth.sh
+++ linhealth.sh
@@ -2,18 +2,11 @@ echo "******************************"
 echo "     System information "
 echo "******************************"
 echo "Operating system : $(uname)"
-
-function host_info(){
-	local dnsips=$(sed -e '/^$/d' /etc/resolv.conf | awk '{if (tolower($1)=="nameserver") print $2}')
-	ipvar= ifconfig eth0 | grep "inet addr"
-	echo "******************************"
-	echo " Hostname and DNS information "
-	echo "******************************"
-	echo "Hostname : $(hostname -s)"
-	echo "DNS domain : $(hostname -d)"
-	echo "Fully qualified domain name : $(hostname -f)"
-	echo "Network address (IP) :" $ipvar
-	echo "DNS name servers (DNS IP) : ${dnsips}"
-}
-
-host_info ;
+echo "******************************"
+echo "     eth0 Configuration "
+echo "******************************"
+ipvar= ifconfig eth0 | grep "inet addr"
+echo "******************************"
+echo "          Hostname "
+echo "******************************"
+echo "      "$hostname
